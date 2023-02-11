44d41ffc816ede7c8f148b348d48b1ede004ea44
ClaudiaCavalleri <cavallericlaudia89@gmail.com>
2023-02-11 14:30:58 +0100

    little changes

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <nav>
-            <small>Developed by © Claudia Cavalleri with <a href="https://it.reactjs.org" target="_blank" rel="noreferrer">React</a></small>
+            <small>Developed by © Claudia Cavalleri with <a href="https://it.reactjs.org" target="_blank" rel="noreferrer">React.JS</a></small>
         </nav>
     )
 }
